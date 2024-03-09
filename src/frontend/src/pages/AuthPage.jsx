eeeb5685f5d7ecd85f6a56a4d741fe9eb9576e1f
InfaNaSotku <gerasimov.mr@petrogm.com>
2024-03-09 16:47:28 +0700

    Added redirection/Deletex extra

--- src/frontend/src/pages/AuthPage.jsx
+++ src/frontend/src/pages/AuthPage.jsx
@@ -1,4 +1,11 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
 export default function AuthPage() {
+    const navigate = useNavigate()
+    const { authData, setAuthData } = useContext(AuthContext)
+
     return (
         <div>Hello world from auth!</div>
     )
