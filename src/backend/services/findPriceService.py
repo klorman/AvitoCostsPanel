b49e69a0dbdee63ed4b758016ff9ae958efda541
Манчук Никита Александрович <n.manchuk@okbsapr.ru>
2024-03-11 13:51:29 +0300

    Исправил поиск цен

--- src/backend/services/findPriceService.py
+++ src/backend/services/findPriceService.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional
 
 from src.backend.models import PriceMatrix, Category, Location, DiscountPriceMatrix, Segment
 from sqlalchemy.orm import Session
@@ -7,64 +7,53 @@ from sqlalchemy.orm import Session
 class FindPriceService:
 
     @staticmethod
-    def get_parent_locations(db: Session, location_id: int) -> List[int]:
-        parent_ids = []
-        current_location = db.query(Location).filter(Location.id == location_id).first()
-
-        while current_location and current_location.parent_id:
-            parent_ids.append(current_location.parent_id)
-            current_location = db.query(Location).filter(Location.id == current_location.parent_id).first()
-
-        return parent_ids
-
+    def get_parent_location(db: Session, location_id: int) -> Optional[int]:
+        return db.query(Location.parent_id).filter(Location.id == location_id).scalar()
+    
     @staticmethod
-    def get_parent_categories(db: Session, category_id: int) -> List[int]:
-        parent_ids = []
-        current_category = db.query(Category).filter(Category.id == category_id).first()
-
-        while current_category and current_category.parent_id:
-            parent_ids.append(current_category.parent_id)
-            current_category = db.query(Category).filter(Category.id == current_category.parent_id).first()
-
-        return parent_ids
+    def get_parent_category(db: Session, category_id: int) -> Optional[int]:
+        return db.query(Category.parent_id).filter(Category.id == category_id).scalar()
 
     def find_price(self, db: Session, location_id: int, category_id: int, user_segment_id: int) -> Optional[dict]:
-        locations_to_check = [location_id] + self.get_parent_locations(db, location_id)
-        categories_to_check = [category_id] + self.get_parent_categories(db, category_id)
+        location_to_check = location_id
+        category_to_check = category_id
 
         # Получаем ID скидочных матриц для сегмента пользователя
-        discount_matrices_ids = db.query(Segment.discount_matrix_id).filter(Segment.id == user_segment_id).all()
+        discount_matrices_ids = db.query(Segment.discount_matrix_id).filter(Segment.id == user_segment_id).order_by(Segment.discount_matrix_id.desc()).all()
         discount_matrices_ids = [dm[0] for dm in discount_matrices_ids]  # Преобразование в список ID
 
-        for loc_id in locations_to_check:
-            for cat_id in categories_to_check:
+        while location_to_check:
+            while category_to_check:
                 # Проверяем скидочную матрицу для сегмента пользователя
                 discount_matrix_price = db.query(DiscountPriceMatrix).filter(
                     DiscountPriceMatrix.id.in_(discount_matrices_ids),
-                    DiscountPriceMatrix.location_id == loc_id,
-                    DiscountPriceMatrix.category_id == cat_id
+                    DiscountPriceMatrix.location_id == location_to_check,
+                    DiscountPriceMatrix.category_id == category_to_check
                 ).first()
                 if discount_matrix_price:
                     return {
                         "price": discount_matrix_price.price,
-                        "location_id": loc_id,
-                        "category_id": cat_id,
+                        "location_id": location_to_check,
+                        "category_id": category_to_check,
                         "matrix_id": discount_matrix_price.id,
                         "user_segment_id": user_segment_id
                     }
+                category_to_check = self.get_parent_category(db, category_to_check)
+            location_to_check = self.get_parent_location(db, location_to_check)
 
-                # Если в скидочной нет, то ищем в базовой
-                base_matrix_price = db.query(PriceMatrix).filter(
-                    PriceMatrix.location_id == loc_id,
-                    PriceMatrix.category_id == cat_id
-                ).first()
-                if base_matrix_price:
-                    return {
-                        "price": base_matrix_price.price,
-                        "location_id": loc_id,
-                        "category_id": cat_id,
-                        "matrix_id": base_matrix_price.id,
-                        "user_segment_id": user_segment_id
-                    }
+        # Если в скидочных нет, то ищем в базовой
+        base_matrix_price = db.query(PriceMatrix).filter(
+            PriceMatrix.location_id == location_id,
+            PriceMatrix.category_id == category_id
+        ).first()
+        if base_matrix_price:
+            return {
+                "price": base_matrix_price.price,
+                "location_id": location_id,
+                "category_id": category_id,
+                "matrix_id": base_matrix_price.id,
+                "user_segment_id": user_segment_id
+            }
 
+        # Если в базовой, вдруг, нет, то возвращаем None
         return None
