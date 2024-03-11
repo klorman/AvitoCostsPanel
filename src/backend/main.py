87e481414109d5c713603decab25b2a264bbde1c
show0ff <nr.freeman20012@gmail.com>
2024-03-12 01:23:19 +0300

    add new endpoints

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,16 +1,18 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy import func
-from sqlalchemy.orm import Session, session
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
-from models import engine, UserAvito, BaselineMatrices, DiscountMatrices
+from models import engine, UserAvito, BaselineMatrices, DiscountMatrices, Location
 from services.findPriceService import FindPriceService
+from services.price_crud_service import PriceCRUDService
 
 app = FastAPI()
 find_price_service = FindPriceService()
+crud_service = PriceCRUDService()
 
 
 class PriceQuery(BaseModel):
@@ -25,11 +27,21 @@ class PriceResponse(BaseModel):
     category_id: int
     user_segment_id: int
 
+
+class MatrixResponse(BaseModel):
+    matrix_id: int
+    matrix_type: str
+
+
+class LocationResponse(BaseModel):
+    loc_id: int
+    loc_name: str
+
+
 # Заменить на цифры
 class MatrixType(str, Enum):
-    Base = "Base"
-    Discount = "Discount"
-
+    Base = 0
+    Discount = 1
 
 
 def get_db():
@@ -40,7 +52,7 @@ def get_db():
         db.close()
 
 
-@app.post("/get-price", response_model=PriceResponse)
+@app.get("/get-price", response_model=PriceResponse)
 def get_price(query: PriceQuery, db: Session = Depends(get_db)):
     user = db.query(UserAvito).filter(UserAvito.user_id == query.user_id).first()
     if not user:
@@ -52,7 +64,6 @@ def get_price(query: PriceQuery, db: Session = Depends(get_db)):
         return PriceResponse(**price_info)
 
     raise HTTPException(status_code=404, detail="Price not found")
-
 
 
 @app.get("/get_matrix")
@@ -70,6 +81,46 @@ def get_matrix(matrix_type: MatrixType, id: Optional[int] = None, db: Session = 
         {"id": matrix.id, "location_id": matrix.location_id, "category_id": matrix.category_id, "price": matrix.price}
         for matrix in result]
 
+
+@app.get("/count_prices_test")
+def count_prices(db: Session = Depends(get_db), id: Optional[int] = None):
+    crud_service.init_calculated_prices(db, id)
+
+
+@app.get("/get_new_price_func_test")
+def get_new_price_func(db: Session = Depends(get_db), location: Optional[int] = None, category: Optional[int] = None,
+                       user_id: Optional[int] = None):
+    price = crud_service.get_price(db, location, category, user_id)
+    return [
+        {
+            "price": price
+        }
+    ]
+
+
+@app.get("/matrix", response_model=List[MatrixResponse])
+def get_matrix(db: Session = Depends(get_db)):
+    baseline_matrices = db.query(BaselineMatrices.matrix_id).distinct().all()
+    discount_matrices = db.query(DiscountMatrices.matrix_id).distinct().all()
+    result = [{"matrix_id": m[0], "matrix_type": "Base"} for m in baseline_matrices] + \
+             [{"matrix_id": m[0], "matrix_type": "Discount"} for m in discount_matrices]
+    return result
+
+
+@app.get("/location", response_model=List[LocationResponse])
+def get_location(matrix_id: int, matrix_type: int, db: Session = Depends(get_db)):
+    if matrix_type == 0:
+        matrix_model = BaselineMatrices
+    elif matrix_type == 1:
+        matrix_model = DiscountMatrices
+    else:
+        raise HTTPException(status_code=400, detail="Invalid matrix type")
+
+    locations = db.query(matrix_model.location_id, Location.name).join(Location,
+                                                                       matrix_model.location_id == Location.id).filter(
+        matrix_model.matrix_id == matrix_id).distinct().all()
+
+    return [{"loc_id": loc[0], "loc_name": loc[1]} for loc in locations]
 
 # @app.get("/get_locs", response_model=List[dict])
 # def get_locs(request: GetLocationsRequest, db: Session = Depends(get_db)):
--- src/backend/services/price_crud_service.py
+++ src/backend/services/price_crud_service.py
@@ -1,6 +1,7 @@
-from typing import Optional, list
+from typing import Optional, List
 
-from src.backend.models import BaselineMatrices, Category, Location, DiscountMatrices, Segment, UserAvito, CalculatedPrices
+from src.backend.models import BaselineMatrices, Category, Location, DiscountMatrices, Segment, UserAvito, \
+    CalculatedPrices
 from sqlalchemy.orm import Session
 
 
@@ -9,47 +10,51 @@ class PriceCRUDService:
     @staticmethod
     def get_parent_location(db: Session, location_id: int) -> Optional[int]:
         return db.query(Location.parent_id).filter(Location.id == location_id).scalar()
-    
 
     @staticmethod
-    def check_is_category_parents_discount_exists(db: Session, location_id: int, category_id: int, discount_matrix_id: int) -> bool:
+    def check_is_category_parents_discount_exists(db: Session, location_id: int, category_id: int,
+                                                  discount_matrix_id: int) -> bool:
         discount_exists = db.query(DiscountMatrices).filter(
-                DiscountMatrices.location_id == location_id,
-                DiscountMatrices.category_id == category_id,
-                DiscountMatrices.matrix_id == discount_matrix_id
+            DiscountMatrices.location_id == location_id,
+            DiscountMatrices.category_id == category_id,
+            DiscountMatrices.matrix_id == discount_matrix_id
         ).first()
         if discount_exists:
             return True
-        
+
         parent_id = PriceCRUDService.get_parent_category(db, category_id)
         if parent_id:
             return False
-        
-        return PriceCRUDService.check_is_category_parents_discount_exists(db, location_id, parent_id, discount_matrix_id)
 
+        return PriceCRUDService.check_is_category_parents_discount_exists(db, location_id, parent_id,
+                                                                          discount_matrix_id)
 
     @staticmethod
-    def find_locations_for_price_update(db: Session, location_id: int, category_id: int, discount_matrix_id: int) -> list[int]:
+    def find_locations_for_price_update(db: Session, location_id: int, category_id: int, discount_matrix_id: int) -> \
+            List[int]:
         child_locations = db.query(Location).filter(Location.parent_id == location_id).all()
         valid_child_ids = []
 
         for location in child_locations:
-            discount_exists = PriceCRUDService.check_is_category_parents_discount_exists(db, location.id, category_id, discount_matrix_id)
+            discount_exists = PriceCRUDService.check_is_category_parents_discount_exists(db, location.id, category_id,
+                                                                                         discount_matrix_id)
 
             if discount_exists:
                 continue
 
             valid_child_ids.append(location.id)
-            valid_child_ids.extend(PriceCRUDService.find_locations_for_price_update(db, location.id, category_id, discount_matrix_id))
+            valid_child_ids.extend(
+                PriceCRUDService.find_locations_for_price_update(db, location.id, category_id, discount_matrix_id))
 
+        return valid_child_ids
 
     @staticmethod
     def get_parent_category(db: Session, category_id: int) -> Optional[int]:
         return db.query(Category.parent_id).filter(Category.id == category_id).scalar()
 
-
     @staticmethod
-    def find_category_child_for_price_update(db: Session, category_id: int, location_id: int, discount_matrix_id: int) -> list[int]:
+    def find_category_child_for_price_update(db: Session, category_id: int, location_id: int,
+                                             discount_matrix_id: int) -> List[int]:
         child_categories = db.query(Category).filter(Category.parent_id == category_id).all()
         valid_child_ids = []
 
@@ -64,12 +69,15 @@ class PriceCRUDService:
                 continue
 
             valid_child_ids.append(category.id)
-            valid_child_ids.extend(PriceCRUDService.find_category_child_for_price_update(db, category.id, category_id, discount_matrix_id))
-  
+            valid_child_ids.extend(
+                PriceCRUDService.find_category_child_for_price_update(db, category.id, category_id, discount_matrix_id))
 
-    def find_price_by_discount_matrix(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int) -> Optional[dict]:
+        return valid_child_ids
+
+    def find_price_by_discount_matrix(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int) -> \
+            Optional[dict]:
         location_to_check = location_id
-        
+
         while location_to_check:
             category_to_check = category_id
             while category_to_check:
@@ -81,7 +89,7 @@ class PriceCRUDService:
                 ).first()
                 if discount_matrix:
                     return discount_matrix.price
-                
+
                 category_to_check = self.get_parent_category(db, category_to_check)
             location_to_check = self.get_parent_location(db, location_to_check)
 
@@ -97,50 +105,54 @@ class PriceCRUDService:
         # Если в базовой, вдруг, нет, то возвращаем None
         return None
 
-
-    def update_calculated_child_prices(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int, price: int, autocommit: bool = True):
-        locations_ids = [location_id] + self.find_locations_for_price_update(db, location_id, category_id, discount_matrix_id)
+    def update_calculated_child_prices(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int,
+                                       price: int, autocommit: bool = True):
+        locations_ids = [location_id] + self.find_locations_for_price_update(db, location_id, category_id,
+                                                                             discount_matrix_id)
 
         for current_location_id in locations_ids:
-            categories_ids = [category_id] + self.find_category_child_for_price_update(db, category_id, current_location_id, discount_matrix_id)
+            categories_ids = [category_id] + self.find_category_child_for_price_update(db, category_id,
+                                                                                       current_location_id,
+                                                                                       discount_matrix_id)
             for current_category_id in categories_ids:
                 calculated_price = db.query(CalculatedPrices).filter(
                     CalculatedPrices.discount_matrix_id == discount_matrix_id,
                     CalculatedPrices.location_id == current_location_id,
                     CalculatedPrices.category_id == current_category_id
                 ).first()
-                calculated_price.price = price
+
+                if calculated_price:
+                    calculated_price.price = price
 
         if autocommit:
             db.commit()
 
-
-    def create_discount_price(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int, price: int):
+    def create_discount_price(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int,
+                              price: int):
         new_discount_price = DiscountMatrices(
-            matrix_id = discount_matrix_id,
-            location_id = location_id,
-            category_id = category_id,
-            price = price
+            matrix_id=discount_matrix_id,
+            location_id=location_id,
+            category_id=category_id,
+            price=price
         )
-        db.add(new_discount_price) # Если уже есть, будет ошибка
-        #db.commit()
+        db.add(new_discount_price)  # Если уже есть, будет ошибка
+        # db.commit()
 
         self.update_calculated_child_prices(db, location_id, category_id, discount_matrix_id, price)
 
-
-    def update_discount_price(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int, price: int):
+    def update_discount_price(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int,
+                              price: int):
         discount_price = db.query(DiscountMatrices).filter(
             DiscountMatrices.matrix_id == discount_matrix_id,
             DiscountMatrices.location_id == location_id,
             DiscountMatrices.category_id == category_id
         ).first()
-        
+
         if discount_price:
             discount_price.price = price
-            #db.commit()
+            # db.commit()
 
             self.update_calculated_child_prices(db, location_id, category_id, discount_matrix_id, price)
-
 
     def delete_discount_price(self, db: Session, location_id: int, category_id: int, discount_matrix_id: int):
         discount_price = db.query(DiscountMatrices).filter(
@@ -148,14 +160,13 @@ class PriceCRUDService:
             DiscountMatrices.location_id == location_id,
             DiscountMatrices.category_id == category_id
         ).first()
-        
+
         if discount_price:
             db.delete(discount_price)
-            #db.commit()
+            # db.commit()
 
             price = self.find_price_by_discount_matrix(db, location_id, category_id, discount_matrix_id)
             self.update_calculated_child_prices(db, location_id, category_id, discount_matrix_id, price)
-
 
     def init_calculated_prices(self, db: Session, baseline_matrix_id: int):
         self.baseline_matrix_id = baseline_matrix_id
@@ -166,17 +177,18 @@ class PriceCRUDService:
         discount_matrices = db.query(DiscountMatrices).all()
 
         for line in discount_matrices:
-            self.update_calculated_child_prices(db, line.location_id, line.category_id, line.matrix_id, autocommit=False)
+            self.update_calculated_child_prices(db, line.location_id, line.category_id, line.matrix_id, line.price,
+                                                autocommit=False)
 
         db.commit()
-            
 
     def get_price(self, db: Session, location_id: int, category_id: int, user_id: int) -> Optional[int]:
         # Получаем ID сегментов пользователя
         user_segments_ids = db.query(UserAvito.segment_id).filter(UserAvito.user_id == user_id).all()
 
         # Получаем ID скидочных матриц для сегмента пользователя
-        discount_matrices_ids = db.query(Segment.discount_matrix_id).filter(Segment.id.in_(user_segments_ids)).order_by(Segment.id.desc()).all()
+        discount_matrices_ids = db.query(Segment.discount_matrix_id).filter(Segment.id.in_(user_segments_ids)).order_by(
+            Segment.id.desc()).all()
         discount_matrices_ids = [dm[0] for dm in discount_matrices_ids]  # Преобразование в список ID
 
         for discount_matrix_id in discount_matrices_ids:
@@ -189,6 +201,5 @@ class PriceCRUDService:
                 return {
                     "price": calculated_price
                 }
-        
+
         return None
-    
